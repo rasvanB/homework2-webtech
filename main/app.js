3e1892a3b400fd2887c2f544345fa57a7cc97f20
Razvan <not.obelix@gmail.com>
2022-11-14 20:55:31 +0200

    finished problem

--- main/app.js
+++ main/app.js
@@ -1,9 +1,26 @@
-function addTokens(input, tokens){
-    
+const isString = (input) => {
+  if (typeof input !== "string" && !(input instanceof String)) return false;
+  return true;
+};
+
+function addTokens(input, tokens) {
+  if (!isString(input)) throw new TypeError("Invalid input");
+
+  if (input.length < 6)
+    throw new Error("Input should have at least 6 characters");
+
+  for (const e of tokens) {
+    if (!("tokenName" in e) || typeof e.tokenName !== "string")
+      throw new Error("Invalid array format");
+    else {
+      input = input.replace("...", "${" + e.tokenName + "}");
+    }
+  }
+  return input;
 }
 
 const app = {
-    addTokens: addTokens
-}
+  addTokens: addTokens,
+};
 
 module.exports = app;
